9bfcee455555e0053e194e677f64563c092a8928
notax3l <singh15tanishq@gmail.com>
2025-03-21 16:10:46 -0400

    new flavours

--- lesson-10/js/json.js
+++ lesson-10/js/json.js
@@ -8,7 +8,7 @@ async function populate(){
 
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    const requestURL = "js/i-scream.json";
+    const requestURL = "https://github.com/KSilver7/comp1073-25w/blob/main/lesson-10/js/i-scream.json";
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
